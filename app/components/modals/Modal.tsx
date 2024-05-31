4c2abdae31bda9a8df104182eb37e990d06b282a
FVidalYepz <Frananyelvidalyepez@gmail.com>
2024-05-31 12:22:18 -0400

    Update Modal.tsx

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -84,19 +84,18 @@ const Modal: React.FC<ModalProps> = ({
 
   return (
     <>
-      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70 ">
+      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto  fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70">
         {" "}
-        <div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full lg:h-auto md:h-auto">
+        <div className=" relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full md:min-h-screen ">
           {/* CONTENT */}
           <div
-            className={`translate duration-300 h-full 
-          ${showModal ? "translate-y-0" : "translate-y-full"} ${
-              showModal ? "opacity-100" : "opacity-0"
-            }`}
+            className={`translate duration-300 h-full content-center
+           ${showModal ? "translate-y-0" : "translate-y-full"} 
+           ${showModal ? "opacity-100" : "opacity-0"}`}
           >
             <div className="translate h-fit lg:h-auto md:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
               {/* HEADER */}
-              <header className="flex items-center p-6 rounded-t justify-center relative border-b">
+              <div className="flex items-center p-6 rounded-t justify-center relative border-b">
                 <button
                   onClick={handleClose}
                   className="p-1 border-0 hover:opacity-70 transition absolute left-9"
@@ -104,7 +103,7 @@ const Modal: React.FC<ModalProps> = ({
                   <IoMdClose size={18} />
                 </button>
                 <div className="text-lg font-semibold">{title}</div>
-              </header>
+              </div>
               {/* BODY */}
               <div className="relative p-6 flex-auto">{body}</div>
               {/* FOOTER */}
